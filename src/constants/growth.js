d4a5adcd69f07ede0da1ffd1ab4e05330a52fd3c
samsperd <iloejeobinna@gmail.com>
2023-12-12 19:22:16 +0100

    first commit

---
+++ src/constants/growth.js
@@ -0,0 +1,22 @@
+export const growth = [
+  {
+    field: "500 million products",
+    description: "available to shop across the catalog",
+    time: 1000
+  },
+  {
+    field: "40,000 stores",
+    description: "from local grocers to chain stores",
+    time: 1300
+  },
+  {
+    field: "5,500+ cities",
+    description: "served across the U.S. & Canada",
+    time: 1600
+  },
+  {
+    field: "Millions of orders",
+    description: "delivered or picked up yearly",
+    time: 1900
+  },
+];
